40bf2be2c183d02e27d756caf856290a622d7ace
Jakub Biesiada <biesiadajakub@icloud.com>
2019-05-12 17:55:44 +0200

    Update Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -4,8 +4,8 @@ import './Loader.scss';
 
 const Loader = () => (
   <div className="loader">
-    <div className="loader__bounce--1" />
-    <div className="loader__bounce--2" />
+    <div className="loader__bounce" />
+    <div className="loader__bounce--delayed" />
   </div>
 );
 
