97a320521a56297a7c9d2f781138fc39f2d8be76
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-31 20:15:53 +0100

    chore: update

--- src/pages/api/auth/ [...nextauth].ts
+++ src/pages/api/auth/ [...nextauth].ts
@@ -1,13 +1,13 @@
 import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+// import Providers from 'next-auth/providers';
 
 const options = {
   providers: [
-    Providers.Spotify({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    }),
+    // Providers.Spotify({
+    //   clientId: process.env.SPOTIFY_CLIENT_ID,
+    //   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    // }),
   ],
   session: {
     jwt: true,
