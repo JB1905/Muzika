94db113845918c81f00e9808f1af7ea42d61324f
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-31 19:51:08 +0100

    chore: update

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -6,8 +6,7 @@ const fetcher = <T>(url: string): Promise<T> =>
   axios
     .get(url, {
       headers: {
-        // Authorization: `Bearer ${token}`,
-        Authorization: `Bearer BQAKUQYTQ8SBoh8iteurbUpSGHvag_2_ZvouDrZr7aiQEiVf6AUlgkUJBaEUSs2Vr-pcVFFkApWDmMejmMGJeOXX_RyYAMEktfzUiJS48bZp10cJvpgwEC7pjVdlJrsB4volCDuIAUBQ8nguCFZ2Lqw-COAeeC2RhvUwudSPyV8tSrqfoG4q5Vks0Wqeebv1UN5tAM1e4pOlz8WxIq3Fkkhf1SsT9VUScvOCBwTxXes9NaAyCWoCpNDgZIJl9hpyxhc9auTNNfU6e_xUoA-cnw`,
+        Authorization: `Bearer ${token}`,
       },
     })
     .then((res) => res.data);
