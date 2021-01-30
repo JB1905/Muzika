5ce148d0309d2e8648edf83626a7a7a1662ea482
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-30 16:51:00 +0100

    chore: update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,23 +31,10 @@ export default class MyDocument extends Document {
                 content="black-translucent"
               />
 
-              <meta charSet="utf-8" />
-              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-              {/* <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' /> */}
-              {/* <meta name='description' content='Description' />
-              <meta name='keywords' content='Keywords' /> */}
-              {/* <title>Muzika</title> */}
-
               <link rel="manifest" href="/manifest.json" />
               <link rel="shortcut icon" href="/favicon.ico" />
-              {/* <link href='/favicon.ico' rel='icon' /> */}
-              {/* <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' /> */}
 
-              {/* <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-              <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' /> */}
-              {/* <link rel="apple-touch-icon" href="/logo512.png" /> */}
-              {/* <link rel="apple-touch-icon" href="/apple-icon.png" /> */}
-              {/* <meta name="theme-color" content="#317EFB"/> */}
+              <meta name="apple-mobile-web-app-capable" content="yes" />
 
               <link
                 rel="apple-touch-startup-image"
